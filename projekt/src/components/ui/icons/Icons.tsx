a9c2355537c2115ad199ce13996a444a86bc43a3
Sh3dow-ware <u2486896@rts-365.dk>
2025-09-23 15:53:07 +0200

    Added more icons!

--- projekt/src/components/ui/icons/Icons.tsx
+++ projekt/src/components/ui/icons/Icons.tsx
@@ -1,4 +1,8 @@
 import { FaArrowLeft as ArrowLeft } from "react-icons/fa";
 import { FaArrowRight as ArrowRight } from "react-icons/fa";
+import { FaXTwitter as Twitter } from "react-icons/fa6";
+import { FaInstagram as Instagram} from "react-icons/fa";
+import { FaYoutube as Youtube} from "react-icons/fa";
+import { FaLinkedin as Linkedin} from "react-icons/fa";
 
-export {ArrowRight, ArrowLeft}
+export {ArrowRight, ArrowLeft, Twitter, Instagram, Youtube, Linkedin}
