6ad9c7246de6977e5b14e9143338b505d38b11b1
Sh3dow-ware <u2486896@rts-365.dk>
2025-09-26 15:19:43 +0200

    added signoutAction.ts

--- projekt/src/app/utils/server/signoutAction.ts
+++ projekt/src/app/utils/server/signoutAction.ts
@@ -6,5 +6,6 @@ export async function deleteCookieAction(cookie_key: string) {
   const cookieStore = await cookies()
   if (cookieStore.has(cookie_key)){
     cookieStore.delete(cookie_key)
+    cookieStore.delete("userID")
   }
 }
