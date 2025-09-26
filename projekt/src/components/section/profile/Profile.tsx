8c3fae0999bbe22d878a99b9d03ea462acea4567
Sh3dow-ware <u2486896@rts-365.dk>
2025-09-26 15:18:53 +0200

    Added Profile Component

---
+++ projekt/src/components/section/profile/Profile.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+
+import {ProfileProps} from "@/types/ProfileTypes";
+import "./Profile.sass"
+import {useState} from "react";
+import {Edit} from "@/components/forms/edit/Edit";
+
+export const Profile = ({user}: ProfileProps) => {
+  const [popup, setPopUp] = useState<boolean>(false)
+
+  function handlePopUp() {
+    setPopUp(prevState => !prevState)
+  }
+
+  return (
+      <>
+        <section className={"profile"}>
+          <h2 className={"profile__title"}>Hej, {`${user.firstname} ${user.lastname}`}</h2>
+          <div className={"profile__container"}>
+            <button onClick={handlePopUp} className={"profile__edit"}>Redigere oplysninger</button>
+          </div>
+          {popup && <div className={"profile__popup"}>
+              <button onClick={handlePopUp} className={"profile__unedit"}>X</button>
+              <Edit></Edit>
+          </div>}
+        </section>
+      </>
+  );
+};
