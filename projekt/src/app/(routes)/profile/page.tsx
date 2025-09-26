257a2bd5b857e253f087c0e3cbfca0a4974e703f
Sh3dow-ware <u2486896@rts-365.dk>
2025-09-26 15:18:31 +0200

    Added ProfilePage

--- projekt/src/app/(routes)/profile/page.tsx
+++ projekt/src/app/(routes)/profile/page.tsx
@@ -1,9 +1,38 @@
-const ProfilePage = () => {
-  return (
-      <>
-      <p>Hejsa!</p>
-      </>
-  );
+import {cookies} from "next/headers";
+import {Profile} from "@/components/section/profile/Profile";
+import {ProfileUserProps} from "@/types/ProfileTypes";
+
+const ProfilePage = async () => {
+
+  const cookiesStore = await cookies()
+  const userID = cookiesStore.get("userID")?.value
+  const access_token = cookiesStore.get("access_token")?.value
+
+  try {
+    if (userID && access_token) {
+      const response = await fetch(process.env.API_URL + "users/" + userID, {
+        method: "GET",
+        headers: {
+          "content-type": "application/json",
+          Authorization: `Bearer ${access_token}`
+        }
+      })
+
+      const data: ProfileUserProps = await response.json()
+      return (
+          <>
+            <Profile user={data}></Profile>
+          </>
+      );
+    }
+  } catch (e) {
+    return (
+        <>
+          <section>
+            <p>Der skete en fejl, {(e as Error).message}</p>
+          </section>
+        </>)
+  }
 };
 
 export default ProfilePage
